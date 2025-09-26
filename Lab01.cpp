196f35c9b00d7991a4a26d972f5c4dc156403e18
RudDavid <satorugodze17@gmail.com>
2025-09-26 16:58:56 +0700

    Update Lab01.cpp

--- Lab01.cpp
+++ Lab01.cpp
@@ -1,45 +1,50 @@
 /****************************
  * Автор:       Руднев Д.А. *
  * Дата:        18.09.2025  *
- * Название:    Вариант 155551  *
+ * Название:    Вариант 11  *
  * **************************/
- 
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-int main(){
-  const double PI = 3.141592;                       // Вводим число пи
-  
+int main() {
+  // Вводим число пи
+  const double PI = 3.141592;
+
   float T;
   float h;
   float v;
   float H;
   int t;
   float a;
-  
-  cout << "Enter the depth of the shaft: ";         // Задаем глубину шахты(0,85 км)
-  cin  >> H;
-  
-  cout << "Enter the constant a: ";                 // Вводим а = 5,96 (м.с2)
-  cin  >> a;
-  
-  cout << "Enter time: ";                           // Pадаем момент времени, в который необходимо необходимо узнать глубину спускания клетки((10 с)
-  cin  >> t;
-  
-  H = H * 1000;                                     // переводим км в м
-  
-  T = PI * sqrt(H / (2 * a));                       // Определяем продолжительность полного опускания клети
-  h = (H / 2) * (1 - cos(sqrt((2 * a) / H ) * t));  // Определяем глубину спускания
-  v = sqrt((a * H) / 2);
-  
-  cout << fixed;
-  cout.precision(5);
-  cout << T << endl;
-  cout << h << endl;
-  cout << v << endl;
-  
-  return 0;
 
+  // Задаем глубину шахты(0,85 км)
+  cout << "Enter the depth of the shaft: ";
+  cin >> H;
+
+  // Вводим а = 5.96 (м.с2)
+  cout << "Enter the constant a: ";
+  cin >> a;
+
+  // Pадаем момент времени, в который необходимо необходимо узнать глубину спускания клетки((10 с)
+  cout << "Enter time: ";
+  cin >> t;
+
+  // переводим км в м
+  H = H * 1000.0f;
+
+  // Определяем продолжительность полного опускания клети 
+  T = PI * sqrt(H / (2 * a));
+  // Определяем глубину спускания
+  h = (H / 2.0f) * (1 - cos(sqrt((2.0f * a) / H) * t));
+  v = sqrt((a * H) / 2);
+
+  cout << fixed; cout.precision(5);
+  cout << T << endl
+    << h << endl
+    << v << endl;
+
+  return 0;
 }
